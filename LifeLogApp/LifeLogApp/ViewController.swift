b4cbd2b8cd09aaf53b712dcd6cd0aaef2c03b10c
baminami <yuki7827@gmail.com>
2018-12-30 18:56:36 +0900

    プロジェクト作成

---
+++ LifeLogApp/LifeLogApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeLogApp
+//
+//  Created by Apple on 2018/12/30.
+//  Copyright © 2018年 Baminami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
